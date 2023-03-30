df796cf69fee3e05c34768e3b059d4a9051a4cd2
Daniel <hss1_xp@hotmail.com>
2023-03-29 23:19:17 -0300

    initial

---
+++ js/main.js
@@ -0,0 +1,60 @@
+(function($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function() {
+        setTimeout(function() {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+
+
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+
+
+    // Back to top button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    function hashchanged() {
+        if (location.hash.substr(1) != '') $('html,body').animate({ scrollTop: ($('#' + location.hash.substr(1)).position().top - 230) }, 'slow');
+    }
+    $(window).on('hashchange', function (e) {
+        hashchanged();
+    });
+    hashchanged();
+    
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+    
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 230
+        }, 200);
+    });
+
+
+
+})(jQuery);
